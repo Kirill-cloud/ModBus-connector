db5246e01c3c1b0c7f3998db10686da83e8bc4ef
Valeria Berezikova <degirl450@gmail.com>
2021-10-30 13:58:52 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ModBus_connector.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModBus_connector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private byte[] tcpSynClBuffer = new byte[2048];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectionService connectionService = new();
+            var x = connectionService.Connect("127.0.0.1",502);
+            x.Send(new byte[12]{ 0,2,0,0,0,6,1,2,0,0,0,1}, 0, 12, SocketFlags.None);
+            int result = x.Receive(tcpSynClBuffer, 0, tcpSynClBuffer.Length, SocketFlags.None);
+
+        }
+    }
+}
---
+++ Services/ConnectionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModBus_connector.Services
+{
+    class ConnectionService
+    {
+        private int _timeout=500;
+
+        public Socket Connect(string ip, int port)
+        {
+            IPAddress _ip;
+            if (IPAddress.TryParse(ip, out _ip) == false)
+            {
+                IPHostEntry hst = Dns.GetHostEntry(ip);
+                ip = hst.AddressList[0].ToString();
+            }
+            var tcpAsyCl = new Socket(IPAddress.Parse(ip).AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            tcpAsyCl.Connect(new IPEndPoint(_ip, port));
+            tcpAsyCl.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendTimeout, _timeout);
+            tcpAsyCl.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, _timeout);
+            tcpAsyCl.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, 1);
+            return tcpAsyCl;
+        }
+    }
+}
