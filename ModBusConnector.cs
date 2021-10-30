3c3ebb1b6505b58c76aad2b414dadbd7967968cb
Konovalenko Kirill <kir6941@gmail.com>
2021-10-30 14:52:32 +0700

    wet back ready

--- ModBusConnector.cs
+++ ModBusConnector.cs
@@ -11,23 +11,23 @@ namespace ModBus_connector
     public class ModBusConnector
     {
         private IConnectionService connectionService;
-        private string ip = "127.0.0.1";
+        private byte[] readInputsCommand = new byte[12] { 0, 2, 0, 0, 0, 6, 1, 2, 0, 0, 0, 1 };
+        private byte[] tcpSynClBuffer = new byte[2048];
 
+        private string ip = "127.0.0.1";
         public string Ip
         {
             get { return ip; }
             set { ip = value; }
         }
+        
         private int port = 502;
-
         public int Port
         {
             get { return port; }
             set { port = value; }
         }
-
-        private byte[] tcpSynClBuffer = new byte[2048];
-
+        
         public ModBusConnector(IConnectionService connectionService)
         {
             this.connectionService = connectionService;
@@ -36,9 +36,12 @@ namespace ModBus_connector
         public bool GetValue()
         {
             var socket = connectionService.Connect(Ip, Port);
-            socket.Send(new byte[12] { 0, 2, 0, 0, 0, 6, 1, 2, 0, 0, 0, 1 }, 0, 12, SocketFlags.None);
+            socket.Send(readInputsCommand, 0, readInputsCommand.Length, SocketFlags.None);
             int result = socket.Receive(tcpSynClBuffer, 0, tcpSynClBuffer.Length, SocketFlags.None);
-            return true;
+            
+            var data = new byte[tcpSynClBuffer[8]];
+            Array.Copy(tcpSynClBuffer, 9, data, 0, tcpSynClBuffer[8]);
+            return Convert.ToBoolean(data[0]);
         }
     }
 }
